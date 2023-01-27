ff0fdc0f45235f3fec16fbf80003bff6556616d8
hzemzem <hajar.zemzem@gmail.com>
2023-01-27 13:39:54 -0500

    fixed maybe

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ function Homepage() {
     return (
         <div className='container homepage'>
             <a href='/search-by-text'>Search by ID, Profession, or Country</a>
-            <a href='search-by-date'>Search for users by date range</a>
+            <a href='search-by-date'>Search by date range</a>
         </div>
     );
 }
